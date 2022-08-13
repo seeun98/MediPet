4aea0a6db9f9b154d9e1c6f5ffb0640e1aeb1d90
eunsoo <eunsoo999@naver.com>
2022-08-13 17:38:35 +0900

    프로젝트 생성 및 Test

---
+++ src/main/java/com/dev/mediPet/MediPetApplication.java
@@ -0,0 +1,13 @@
+package com.dev.mediPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/mediPet/MediPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.mediPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
