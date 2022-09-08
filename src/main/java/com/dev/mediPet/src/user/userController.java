7ff0bb391e71b07862b83611ddaa113e0904d257
seeun98 <michelle5659@naver.com>
2022-09-09 00:23:20 +0900

    JSONParser import 고침

--- src/main/java/com/dev/mediPet/src/user/userController.java
+++ src/main/java/com/dev/mediPet/src/user/userController.java
@@ -17,15 +17,7 @@ import static com.dev.mediPet.config.BaseResponseStatus.AUTH_KAKAO_EMPTY_TOKEN;
 @RequestMapping("/user")
 public class userController {
 
-//    @Autowired
-//    //카카오로그인 Get 프론트로부터 토큰 받았다고 가정 -> 토큰 번호 확인
-//    @ResponseBody
-//    @GetMapping("/login/GetKakao")
-//    @JsonProperty("code")
-//    public void kakaoCallback(@RequestParam String code)
-//    {
-//        System.out.println(code);
-//    }
+
 
     /**
      * 3. 카카오 로그인 API
@@ -34,18 +26,16 @@ public class userController {
     @ResponseBody
     @PostMapping("/login/kakao")
     public String kakaoLogin(@RequestBody PostKakaoLoginReq postKakaoLogin) {
-        System.out.println("컨트롤러들어옴");
+
         if (postKakaoLogin.getAccessToken() == null || postKakaoLogin.getAccessToken().isEmpty()) {
-            return "getAccessToken FAILED";
+            return "FAILED";
             //return new BaseResponse<>(AUTH_KAKAO_EMPTY_TOKEN);
         }
 
         try{
-            System.out.println("try");
             KakaoUserInfo kakaoUserInfo = KakaoApiService.getKakaoUserInfo(postKakaoLogin.getAccessToken());
-            // TODO 지우기
-            System.out.println("끝남");
             System.out.println(kakaoUserInfo);
+
         } catch (BaseException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/dev/mediPet/src/user/utils/KakaoApiService.java
+++ src/main/java/com/dev/mediPet/src/user/utils/KakaoApiService.java
@@ -2,7 +2,7 @@ package com.dev.mediPet.src.user.utils;
 
 import com.dev.mediPet.config.BaseException;
 import com.dev.mediPet.src.user.model.KakaoUserInfo;
-import org.apache.tomcat.util.json.JSONParser;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
 
 
@@ -26,8 +26,6 @@ kakao developers / 사용자 정보 가져오기 (https://developers.kakao.com/d
 * */
 public class KakaoApiService {
     public static KakaoUserInfo getKakaoUserInfo(String accessToken) throws BaseException {
-        // TODO 지우기
-        System.out.println("카카오 들어옴");
         String api = "https://kapi.kakao.com/v2/user/me"; //기본정보
         String authorization_header = "Bearer " + accessToken;
         KakaoUserInfo kakaoUserInfo; // 정보 받아서 넘겨줄 변수
@@ -39,13 +37,11 @@ public class KakaoApiService {
             URLConnection : 사용자 인증이나 보안이 설정되어 있지 않은 웹서버에 접속하여 파일 등을 다운로드하는데 사용, 재사용될 수 없음
             HttpsURLConnection : Java 소스 내에서 SSL 적용된 사이트에 접근하기 위해, REST api를 호출하기 위해 사용.
             * */
-// TODO 지우기
-            System.out.println("두번째");
+
             http.setRequestProperty("Authorization", authorization_header); //Request Header 값 세팅 (response data를 2번째값으로 전달)
             http.setRequestMethod("GET");
             http.connect();
-// TODO 지우기
-            System.out.println("3번째");
+
             int responseCode = http.getResponseCode(); //http response 404 에러 등등 값 가지기
             System.out.println("responseCode: " + responseCode);
 
@@ -55,29 +51,26 @@ public class KakaoApiService {
             while ((line = br.readLine()) != null) {
                 sb.append(line).append("\n");
             }
-// TODO 지우기
-            System.out.println("dd  " + sb.toString());
-            JSONParser parser = new JSONParser(sb.toString());
+
+            JSONParser parser = new JSONParser(); //아악 ... import 문제였던 것 ㅠㅠ
             JSONObject jsonObjectMain;
-            System.out.println("here1");
-            //TODO
+            jsonObjectMain = (JSONObject) parser.parse(sb.toString());
             //parse 확인
-            jsonObjectMain = (JSONObject) parser.parse();
+
+
             System.out.println("here");
             JSONObject kakao_account = (JSONObject) jsonObjectMain.get("kakao_account");
             JSONObject profile = (JSONObject) kakao_account.get("profile");
             String nickname = (String) profile.get("nickname");
             //boolean has_email = (boolean) kakao_account.get("email");
             //String email = (String) kakao_account.get("email");
-// TODO 지우기
-            System.out.println("52번째");
+
             System.out.println("nickname : " + nickname);
             br.close();
             in.close();
             http.disconnect();
             kakaoUserInfo = new KakaoUserInfo(nickname);
-            // TODO 지우기
-            System.out.println("5번째");
+
         } catch (Exception exception) {
             throw new BaseException(FAILED_TO_KAKAO_AUTH);
         }
